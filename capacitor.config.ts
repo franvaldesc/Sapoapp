b7a65063c1192ba4ba487c0fe81492de47542f35
Francisco <fran.valdesc@duocuc.cl>
2024-11-24 08:39:03 -0300

    segundo commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sapo.app',
+  appName: 'Sapoapp',
+  webDir: 'dist/angular-18-primeng-app/browser'
+};
+
+export default config;
